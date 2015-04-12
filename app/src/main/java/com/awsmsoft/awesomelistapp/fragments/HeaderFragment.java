35192baf7c6aa992975fc7fd666d022829125e7f
Sier Violencia <sierisimo@gmail.com>
2015-04-12 18:44:14 -0500

    Added a lot of files

---
+++ app/src/main/java/com/awsmsoft/awesomelistapp/fragments/HeaderFragment.java
@@ -0,0 +1,9 @@
+package com.awsmsoft.awesomelistapp.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sierisimo on 12/04/15.
+ */
+public class HeaderFragment extends Fragment {
+}

e6069abd0f96a137fdcc883c348a2cd8aab553e8
Sier Violencia <sierisimo@gmail.com>
2015-04-14 02:43:41 -0500

    Some special names changed, just for readbility - Also added a class an array to the adapter - Changed the names and packages
    * Not sure of what we are going to put in every fragment, but well, he hope that's not the big deal

---
+++ app/src/main/java/com/awsmsoft/awesomelistapp/fragments/tutorial/HomeTutorialFragment.java
@@ -0,0 +1,53 @@
+package com.awsmsoft.awesomelistapp.fragments.tutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.awsmsoft.awesomelistapp.R;
+
+public class HomeTutorialFragment extends Fragment {
+    private String title;
+    private int page;
+
+    private static HomeTutorialFragment mInstance;
+
+    public static HomeTutorialFragment newInstance(int page, String title) {
+        HomeTutorialFragment homeTutorialFragment = new HomeTutorialFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        homeTutorialFragment.setArguments(args);
+        return homeTutorialFragment;
+    }
+
+    public static Fragment getInstance() {
+        if (mInstance == null) {
+            mInstance = new HomeTutorialFragment(); //TODO: Set to a custom constructor where the arguments are passed
+        }
+
+        return mInstance;
+    }
+
+    public HomeTutorialFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someString1");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tutorial_fragment_home, container, false);
+        TextView content = (TextView) view.findViewById(R.id.content);
+        content.setText(page + " -- " + title);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/awsmsoft/awesomelistapp/fragments/tutorial/ProfileTutorialFragment.java
@@ -0,0 +1,58 @@
+package com.awsmsoft.awesomelistapp.fragments.tutorial;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.awsmsoft.awesomelistapp.R;
+
+public class ProfileTutorialFragment extends Fragment {
+    private String title;
+    private int page;
+
+    private static ProfileTutorialFragment mInstance;
+
+    public static ProfileTutorialFragment newInstance(int page, String title) {
+        ProfileTutorialFragment profileTutorialFragment = new ProfileTutorialFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        profileTutorialFragment.setArguments(args);
+        return profileTutorialFragment;
+    }
+
+    public static Fragment getInstance() {
+        if (mInstance == null) {
+            mInstance = new ProfileTutorialFragment();
+        }
+
+        return mInstance;
+    }
+
+    public ProfileTutorialFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            page = getArguments().getInt("someInt", 1);
+            title = getArguments().getString("someTitle");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tutorial_fragment_prodile, container, false);
+        TextView content = (TextView) view.findViewById(R.id.content);
+        content.setText(page + " -- " + title);
+        return view;
+    }
+
+
+}
